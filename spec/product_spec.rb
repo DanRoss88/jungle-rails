19dc07f9d036a53d3ff0567eed17f2be18c54b70
DanRoss88 <danielmartinross@gmail.com>
2023-06-02 08:01:10 -0700

    had to reset db, somehow tests were on the page

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,15 +1,6 @@
 
 require_relative '../config/environment'
-# Each example (it) is run in isolation of others. Therefore each example will needs its own @category created and then @product initialized with that category
-# Create an initial example that ensures that a product with all four fields set will indeed save successfully
-# Have one example for each validation, and for each of these:
-# Set all fields to a value but the validation field being tested to nil
-# Test that the expect error is found within the .errors.full_messages array
-# You should therefore have five examples defined given that you have the four validations above
-#validates :name, presence: true
-# validates :price, presence: true
-# validates :quantity, presence: true
-# validates :category, presence: true
+
 
 RSpec.describe Product, type: :model do
 
